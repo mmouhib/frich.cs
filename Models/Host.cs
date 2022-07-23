a8bd19a2d2ab9170c19e7bb9e9a943c51f1282fd
mouhib <iwritebadcode@github.com>
2022-07-23 17:36:26 +0100

    Add host model

---
+++ Models/Host.cs
@@ -0,0 +1,9 @@
+﻿namespace frich.Models;
+
+public class Host
+{
+    public int HostId { get; set; }
+    public string Username { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
