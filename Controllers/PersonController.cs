561a9065a4f27f0020f0e187d05b6b871a2e7734
mouhib <iwritebadcode@github.com>
2022-07-24 01:25:25 +0100

    Add controller

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using frich.Data;
+using frich.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace frich.Controllers;
+
+[Route("api/persons")]
+[ApiController]
+public class PersonController : ControllerBase
+{
+    private readonly IFrichRepo _repository;
+
+    public PersonController(IFrichRepo repo)
+    {
+        _repository = repo;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Person>> GetAllPlayers()
+    {
+        return Ok(_repository.GetAllPersons());
+    }
+}
