f5486b0bab01b0b8f70bec37646059c9fdcba8f0
mouhib <iwritebadcode@github.com>
2022-07-24 01:09:03 +0100

    Add dbcontext and db connections

---
+++ Data/FrichDbContext.cs
@@ -0,0 +1,14 @@
+﻿using frich.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace frich.Data;
+
+public class FrichDbContext : DbContext
+{
+    public FrichDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+
+    public DbSet<Person> Persons { get; set; } = default!;
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using frich.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 {
     builder.Services.AddControllers();
+    builder.Services.AddTransient<IFrichRepo, MockFrichRepo>();
+
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    builder.Services.AddDbContext<FrichDbContext>(options => options.UseNpgsql(connectionString));
 }
 
 var app = builder.Build();
