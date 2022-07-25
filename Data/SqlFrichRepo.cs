1494392a5bf9ae5886d631ff64f3b19e90934cf1
mouhib <iwritebadcode@github.com>
2022-07-25 02:05:59 +0100

    Replace mock repo with real data repo

---
+++ Data/SqlFrichRepo.cs
@@ -0,0 +1,38 @@
+﻿using frich.Models;
+
+namespace frich.Data;
+
+public class SqlFrichRepo : IFrichRepo
+{
+    private readonly FrichDbContext _context;
+
+    public SqlFrichRepo(FrichDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Person> GetAllPersons()
+    {
+        return _context.Persons.ToList();
+    }
+
+    public Person GetPersonById(int id)
+    {
+        return _context.Persons.FirstOrDefault(person => person.PersonId == id);
+    }
+
+    public void AddPerson(Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeletePerson(Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EditPerson(Person person)
+    {
+        throw new NotImplementedException();
+    }
+}
