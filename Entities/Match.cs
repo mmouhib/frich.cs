f72de1f1ce4f4c245aa92d4792f0ce44e023498c
mouhib <iwritebadcode@github.com>
2022-08-20 16:37:09 +0100

    Remove nullable checks in entities.

--- Entities/Match.cs
+++ Entities/Match.cs
@@ -6,8 +6,8 @@ namespace frich.Entities;
 public class Match
 {
     [Key] public int MatchId { get; set; }
-    [Required] public string MatchType { get; set; } = default!;
-    [Required] public string MatchScore { get; set; } = default!;
+    [Required] public string MatchType { get; set; }
+    [Required] public string MatchScore { get; set; }
     [Required] public DateOnly MatchDate { get; set; }
     [Required] public int Duration { get; set; }
 
--- Entities/Person.cs
+++ Entities/Person.cs
@@ -5,9 +5,9 @@ namespace frich.Entities;
 public class Person
 {
     [Key] public int PersonId { get; set; }
-    [Required] public string Username { get; set; } = default!;
-    [Required] public string Email { get; set; } = default!;
-    [Required] public string Password { get; set; } = default!;
+    [Required] public string Username { get; set; }
+    [Required] public string Email { get; set; }
+    [Required] public string Password { get; set; }
 
     public List<Match> Matches { get; set; }
 
